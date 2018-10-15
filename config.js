b4cdf457166b8ef4d5fe93c7341d0550ff69a018
joey <cewong@ace2u.com>
2018-10-15 23:22:25 +0800

    With HTTPS support for scaffolding

---
+++ config.js
@@ -0,0 +1,27 @@
+/*
+ * Create and export configuration variables.
+ *
+ */
+
+ // Container.
+ var environments = {};
+
+ environments.staging = {
+ 	'httpPort' : 3000,
+ 	'httpsPort' : 3001,
+ 	'envName' : 'staging'
+ };
+
+ environments.production = {
+	'httpPort' : 5000,
+	'httpsPort' : 5001,
+ 	'envName' : 'production'
+ };
+
+
+// Determine which env was passed as command-line arguement
+var currentEnvironment = typeof(process.env.NODE_ENV) == 'string' ? process.env.NODE_ENV.toLowerCase() : '';
+
+var envToExport = typeof(environments[currentEnvironment]) == 'object' ? environments[currentEnvironment] : environments.staging;
+
+module.exports = envToExport;
---
+++ index.js
@@ -0,0 +1,97 @@
+const httplib = require("http");
+const httpslib = require("https");
+const urllib = require("url");
+const StringDecoder = require("string_decoder").StringDecoder;
+var config = require('./config');
+var fs = require('fs');
+
+var httpServer = httplib.createServer(function(req, res) {
+	unifiedServer(req,res);
+});
+
+httpServer.listen(config.httpPort, function() {
+	console.log("Listening now at " + config.envName + " at " + config.httpPort);
+});
+
+
+// Instantiate the HTTPS server.
+var httpsServerOptions = {
+	'key' : fs.readFileSync('./https/key.pem'),
+	'cert' : fs.readFileSync('./https/cert.pem')
+};
+
+var httpsServer = httpslib.createServer(httpsServerOptions, function(req, res) {
+	unifiedServer(req,res);
+});
+// Start the HTTPS Server.
+httpsServer.listen(config.httpsPort, function() {
+	console.log("Listening now at " + config.envName + " at " + config.httpsPort);
+});
+
+// All the server logic for both http and https server
+var unifiedServer = function(req,res) {
+	var parsedUrl = urllib.parse(req.url, true);
+	var path = parsedUrl.pathname;
+	var trimmedPath = path.replace(/^\/+|\/+$/g, '');
+	var queryStringObject = parsedUrl.query;
+	var method = req.method.toLowerCase();
+
+	var headers = req.headers;
+	
+	var decoder = new StringDecoder('utf-8');
+	var buffer = '';
+
+	req.on('data', function(data) {
+		buffer += decoder.write(data);
+	});
+
+	req.on('end', function() {
+		buffer += decoder.end();
+
+		// Choose the handler request should go to.
+		var chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notfound;
+
+		var data = { 'trimmedPath' : trimmedPath,
+		'queryStringObject':queryStringObject,
+		'method': method,
+		'headers': headers,
+		'payload': buffer
+		};
+
+		// Route the request to handler specify in the router
+		chosenHandler(data, function(statusCode, payload) {
+			statusCode = typeof(statusCode) == 'number'? statusCode : 200;
+
+			payload = typeof(payload) == 'object' ? payload : {};
+
+			var payloadString = JSON.stringify(payload);
+
+			res.setHeader('Content-type', 'application/json');
+			res.writeHead(statusCode);
+			res.end(payloadString);
+			console.log('Returning this response :', statusCode, payloadString);
+		});
+	});
+}; 
+
+// Define a handler
+var handlers = {};
+handlers.sample = function(data, callback) {
+ // return a http status code and payload object
+ callback(406, {'name': 'sample handler'});
+};
+
+handlers.notfound = function(data, callback) {
+ callback(404);
+};
+
+handlers.myown = function (data, callback) {
+	callback('202g', {'name':'my own handlers', 'purpose':'testing', 'version':1.20});
+};
+
+// Define a router
+var router = {
+'sample': handlers.sample,
+'deng' : handlers.myown,
+'ping' : handlers.ping
+};
